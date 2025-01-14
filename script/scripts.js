07f29ee2dbec3e875d62a8a5e430bd0a4ceb27d4
Mitchel Mathias <mitchelmathias2904@gmail.com>
2025-01-14 13:33:05 -0300

    atualização do formulário

--- script/scripts.js
+++ script/scripts.js
@@ -1,7 +1,10 @@
-const viewer = document.querySelector('#viewer');
-        viewer.addEventListener('load', () => {
-          const aba_topo = viewer.model.materials[0]; // Acessa o primeiro material
-          const corpo = viewer.model.materials[1]; // Acessa o primeiro material
-          aba_topo.pbrMetallicRoughness.setBaseColorFactor([0.2, 0.5, 0.8, 1.0]); // Define a cor (RGBA)
-          corpo.pbrMetallicRoughness.setBaseColorFactor([0.2, 0.5, 0.8, 1.0]); // Define a cor (RGBA)
-        });
+$(document).ready(function(){
+    const viewer = document.querySelector('#viewer');
+    viewer.addEventListener('load', () => {
+    const aba_topo = viewer.model.materials[0]; // Acessa o primeiro material
+    const corpo = viewer.model.materials[1]; // Acessa o primeiro material
+    aba_topo.pbrMetallicRoughness.setBaseColorFactor([0.2, 0.5, 0.8, 1.0]); // Define a cor (RGBA)
+    corpo.pbrMetallicRoughness.setBaseColorFactor([0.2, 0.5, 0.8, 1.0]); // Define a cor (RGBA)
+});
+
+})
